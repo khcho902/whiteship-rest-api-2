f034fd707d2075cf759ef20b6a2821898eee9878
khcho902 <khcho902@gmail.com>
2022-02-07 16:01:01 +0900

    입력값 제한하기

--- src/main/java/me/kycho/demo/events/EventController.java
+++ src/main/java/me/kycho/demo/events/EventController.java
@@ -1,6 +1,7 @@
 package me.kycho.demo.events;
 
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -18,11 +19,13 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 public class EventController {
 
     private final EventRepository eventRepository;
+    private final ModelMapper modelMapper;
 
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody Event event) {
-        Event newEvent = this.eventRepository.save(event);
+    public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
 
+        Event event = modelMapper.map(eventDto, Event.class);
+        Event newEvent = this.eventRepository.save(event);
         URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
         return ResponseEntity.created(createdUri).body(event);
     }
