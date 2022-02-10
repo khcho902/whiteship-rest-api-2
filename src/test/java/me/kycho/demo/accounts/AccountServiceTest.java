1a65df46e0ceee7427f8ae8cbaffe69588e1a117
khcho902 <khcho902@gmail.com>
2022-02-10 14:08:22 +0900

    스프링 시큐리티 적용

---
+++ src/test/java/me/kycho/demo/accounts/AccountServiceTest.java
@@ -0,0 +1,46 @@
+package me.kycho.demo.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AccountServiceTest {
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    void findByUsername() {
+        // given
+        String username = "kycho@naver.com";
+        String password = "admin1234";
+        Account account = Account.builder()
+                .email(username)
+                .password(password)
+                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                .build();
+
+        this.accountRepository.save(account);
+
+        // when
+        UserDetailsService userDetailsService = (UserDetailsService) this.accountService;
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        // then
+        assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+
+}
